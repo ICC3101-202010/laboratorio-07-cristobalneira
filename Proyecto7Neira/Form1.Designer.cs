4c235758e2fdd4ba051960b598ecffd7c97ffa91
Cristobal Neira <62522973+cristobalneira@users.noreply.github.com>
2020-05-26 11:07:53 -0400

    Primer

---
+++ Proyecto7Neira/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace Proyecto7Neira
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.boton7 = new System.Windows.Forms.Button();
+            this.boton4 = new System.Windows.Forms.Button();
+            this.boton1 = new System.Windows.Forms.Button();
+            this.boton0 = new System.Windows.Forms.Button();
+            this.botonpunto = new System.Windows.Forms.Button();
+            this.boton2 = new System.Windows.Forms.Button();
+            this.boton5 = new System.Windows.Forms.Button();
+            this.boton8 = new System.Windows.Forms.Button();
+            this.botonANS = new System.Windows.Forms.Button();
+            this.boton3 = new System.Windows.Forms.Button();
+            this.boton6 = new System.Windows.Forms.Button();
+            this.boton9 = new System.Windows.Forms.Button();
+            this.botonMAS = new System.Windows.Forms.Button();
+            this.botonIGUAL = new System.Windows.Forms.Button();
+            this.botonX = new System.Windows.Forms.Button();
+            this.botonDEL = new System.Windows.Forms.Button();
+            this.botonMEN = new System.Windows.Forms.Button();
+            this.botonHISTORY = new System.Windows.Forms.Button();
+            this.botonDIV = new System.Windows.Forms.Button();
+            this.botonAC = new System.Windows.Forms.Button();
+            this.textbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // boton7
+            // 
+            this.boton7.BackColor = System.Drawing.Color.Silver;
+            this.boton7.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton7.Location = new System.Drawing.Point(69, 74);
+            this.boton7.Name = "boton7";
+            this.boton7.Size = new System.Drawing.Size(75, 68);
+            this.boton7.TabIndex = 0;
+            this.boton7.Text = "7";
+            this.boton7.UseVisualStyleBackColor = false;
+            this.boton7.Click += new System.EventHandler(this.boton7_Click);
+            // 
+            // boton4
+            // 
+            this.boton4.BackColor = System.Drawing.Color.Silver;
+            this.boton4.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton4.Location = new System.Drawing.Point(69, 148);
+            this.boton4.Name = "boton4";
+            this.boton4.Size = new System.Drawing.Size(75, 68);
+            this.boton4.TabIndex = 1;
+            this.boton4.Text = "4";
+            this.boton4.UseVisualStyleBackColor = false;
+            this.boton4.Click += new System.EventHandler(this.boton4_Click);
+            // 
+            // boton1
+            // 
+            this.boton1.BackColor = System.Drawing.Color.Silver;
+            this.boton1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton1.Location = new System.Drawing.Point(69, 222);
+            this.boton1.Name = "boton1";
+            this.boton1.Size = new System.Drawing.Size(75, 68);
+            this.boton1.TabIndex = 3;
+            this.boton1.Text = "1";
+            this.boton1.UseVisualStyleBackColor = false;
+            this.boton1.Click += new System.EventHandler(this.boton1_Click);
+            // 
+            // boton0
+            // 
+            this.boton0.BackColor = System.Drawing.Color.Silver;
+            this.boton0.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton0.Location = new System.Drawing.Point(69, 296);
+            this.boton0.Name = "boton0";
+            this.boton0.Size = new System.Drawing.Size(75, 68);
+            this.boton0.TabIndex = 2;
+            this.boton0.Text = "0";
+            this.boton0.UseVisualStyleBackColor = false;
+            this.boton0.Click += new System.EventHandler(this.boton0_Click);
+            // 
+            // botonpunto
+            // 
+            this.botonpunto.BackColor = System.Drawing.Color.Silver;
+            this.botonpunto.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonpunto.Location = new System.Drawing.Point(159, 296);
+            this.botonpunto.Name = "botonpunto";
+            this.botonpunto.Size = new System.Drawing.Size(75, 68);
+            this.botonpunto.TabIndex = 7;
+            this.botonpunto.Text = ".";
+            this.botonpunto.UseVisualStyleBackColor = false;
+            this.botonpunto.Click += new System.EventHandler(this.botonpunto_Click);
+            // 
+            // boton2
+            // 
+            this.boton2.BackColor = System.Drawing.Color.Silver;
+            this.boton2.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton2.Location = new System.Drawing.Point(159, 222);
+            this.boton2.Name = "boton2";
+            this.boton2.Size = new System.Drawing.Size(75, 68);
+            this.boton2.TabIndex = 6;
+            this.boton2.Text = "2";
+            this.boton2.UseVisualStyleBackColor = false;
+            this.boton2.Click += new System.EventHandler(this.boton2_Click);
+            // 
+            // boton5
+            // 
+            this.boton5.BackColor = System.Drawing.Color.Silver;
+            this.boton5.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton5.Location = new System.Drawing.Point(159, 148);
+            this.boton5.Name = "boton5";
+            this.boton5.Size = new System.Drawing.Size(75, 68);
+            this.boton5.TabIndex = 5;
+            this.boton5.Text = "5";
+            this.boton5.UseVisualStyleBackColor = false;
+            this.boton5.Click += new System.EventHandler(this.boton5_Click);
+            // 
+            // boton8
+            // 
+            this.boton8.BackColor = System.Drawing.Color.Silver;
+            this.boton8.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton8.Location = new System.Drawing.Point(159, 74);
+            this.boton8.Name = "boton8";
+            this.boton8.Size = new System.Drawing.Size(75, 68);
+            this.boton8.TabIndex = 4;
+            this.boton8.Text = "8";
+            this.boton8.UseVisualStyleBackColor = false;
+            this.boton8.Click += new System.EventHandler(this.boton8_Click);
+            // 
+            // botonANS
+            // 
+            this.botonANS.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.botonANS.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonANS.Location = new System.Drawing.Point(251, 296);
+            this.botonANS.Name = "botonANS";
+            this.botonANS.Size = new System.Drawing.Size(75, 68);
+            this.botonANS.TabIndex = 15;
+            this.botonANS.Text = "ANS";
+            this.botonANS.UseVisualStyleBackColor = false;
+            this.botonANS.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // boton3
+            // 
+            this.boton3.BackColor = System.Drawing.Color.Silver;
+            this.boton3.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton3.Location = new System.Drawing.Point(251, 222);
+            this.boton3.Name = "boton3";
+            this.boton3.Size = new System.Drawing.Size(75, 68);
+            this.boton3.TabIndex = 14;
+            this.boton3.Text = "3";
+            this.boton3.UseVisualStyleBackColor = false;
+            this.boton3.Click += new System.EventHandler(this.boton3_Click);
+            // 
+            // boton6
+            // 
+            this.boton6.BackColor = System.Drawing.Color.Silver;
+            this.boton6.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton6.Location = new System.Drawing.Point(251, 148);
+            this.boton6.Name = "boton6";
+            this.boton6.Size = new System.Drawing.Size(75, 68);
+            this.boton6.TabIndex = 13;
+            this.boton6.Text = "6";
+            this.boton6.UseVisualStyleBackColor = false;
+            this.boton6.Click += new System.EventHandler(this.boton6_Click);
+            // 
+            // boton9
+            // 
+            this.boton9.BackColor = System.Drawing.Color.Silver;
+            this.boton9.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton9.Location = new System.Drawing.Point(251, 74);
+            this.boton9.Name = "boton9";
+            this.boton9.Size = new System.Drawing.Size(75, 68);
+            this.boton9.TabIndex = 12;
+            this.boton9.Text = "9";
+            this.boton9.UseVisualStyleBackColor = false;
+            this.boton9.Click += new System.EventHandler(this.boton9_Click);
+            // 
+            // botonMAS
+            // 
+            this.botonMAS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.botonMAS.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonMAS.Location = new System.Drawing.Point(342, 222);
+            this.botonMAS.Name = "botonMAS";
+            this.botonMAS.Size = new System.Drawing.Size(75, 68);
+            this.botonMAS.TabIndex = 11;
+            this.botonMAS.Text = "+";
+            this.botonMAS.UseVisualStyleBackColor = false;
+            this.botonMAS.Click += new System.EventHandler(this.botonMAS_Click);
+            // 
+            // botonIGUAL
+            // 
+            this.botonIGUAL.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.botonIGUAL.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonIGUAL.Location = new System.Drawing.Point(342, 296);
+            this.botonIGUAL.Name = "botonIGUAL";
+            this.botonIGUAL.Size = new System.Drawing.Size(75, 68);
+            this.botonIGUAL.TabIndex = 10;
+            this.botonIGUAL.Text = "=";
+            this.botonIGUAL.UseVisualStyleBackColor = false;
+            this.botonIGUAL.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // botonX
+            // 
+            this.botonX.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.botonX.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonX.Location = new System.Drawing.Point(342, 148);
+            this.botonX.Name = "botonX";
+            this.botonX.Size = new System.Drawing.Size(75, 68);
+            this.botonX.TabIndex = 9;
+            this.botonX.Text = "x";
+            this.botonX.UseVisualStyleBackColor = false;
+            this.botonX.Click += new System.EventHandler(this.botonx_Click);
+            // 
+            // botonDEL
+            // 
+            this.botonDEL.BackColor = System.Drawing.Color.DarkSalmon;
+            this.botonDEL.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonDEL.Location = new System.Drawing.Point(342, 74);
+            this.botonDEL.Name = "botonDEL";
+            this.botonDEL.Size = new System.Drawing.Size(75, 68);
+            this.botonDEL.TabIndex = 8;
+            this.botonDEL.Text = "DEL";
+            this.botonDEL.UseVisualStyleBackColor = false;
+            this.botonDEL.Click += new System.EventHandler(this.botonDEL_Click);
+            // 
+            // botonMEN
+            // 
+            this.botonMEN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.botonMEN.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonMEN.Location = new System.Drawing.Point(433, 222);
+            this.botonMEN.Name = "botonMEN";
+            this.botonMEN.Size = new System.Drawing.Size(75, 68);
+            this.botonMEN.TabIndex = 19;
+            this.botonMEN.Text = "-";
+            this.botonMEN.UseVisualStyleBackColor = false;
+            this.botonMEN.Click += new System.EventHandler(this.botonRes_Click);
+            // 
+            // botonHISTORY
+            // 
+            this.botonHISTORY.BackColor = System.Drawing.SystemColors.Highlight;
+            this.botonHISTORY.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonHISTORY.Location = new System.Drawing.Point(433, 296);
+            this.botonHISTORY.Name = "botonHISTORY";
+            this.botonHISTORY.Size = new System.Drawing.Size(75, 68);
+            this.botonHISTORY.TabIndex = 18;
+            this.botonHISTORY.Text = "HISTORY";
+            this.botonHISTORY.UseVisualStyleBackColor = false;
+            this.botonHISTORY.Click += new System.EventHandler(this.botonHist_Click);
+            // 
+            // botonDIV
+            // 
+            this.botonDIV.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.botonDIV.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonDIV.Location = new System.Drawing.Point(433, 148);
+            this.botonDIV.Name = "botonDIV";
+            this.botonDIV.Size = new System.Drawing.Size(75, 68);
+            this.botonDIV.TabIndex = 17;
+            this.botonDIV.Text = "÷";
+            this.botonDIV.UseVisualStyleBackColor = false;
+            this.botonDIV.Click += new System.EventHandler(this.botondiv_Click);
+            // 
+            // botonAC
+            // 
+            this.botonAC.BackColor = System.Drawing.Color.DarkSalmon;
+            this.botonAC.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonAC.Location = new System.Drawing.Point(433, 74);
+            this.botonAC.Name = "botonAC";
+            this.botonAC.Size = new System.Drawing.Size(75, 68);
+            this.botonAC.TabIndex = 16;
+            this.botonAC.Text = "AC";
+            this.botonAC.UseVisualStyleBackColor = false;
+            this.botonAC.Click += new System.EventHandler(this.button20_Click);
+            // 
+            // textbox
+            // 
+            this.textbox.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textbox.Location = new System.Drawing.Point(69, 39);
+            this.textbox.Name = "textbox";
+            this.textbox.ReadOnly = true;
+            this.textbox.Size = new System.Drawing.Size(439, 21);
+            this.textbox.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 429);
+            this.Controls.Add(this.textbox);
+            this.Controls.Add(this.botonMEN);
+            this.Controls.Add(this.botonHISTORY);
+            this.Controls.Add(this.botonDIV);
+            this.Controls.Add(this.botonAC);
+            this.Controls.Add(this.botonANS);
+            this.Controls.Add(this.boton3);
+            this.Controls.Add(this.boton6);
+            this.Controls.Add(this.boton9);
+            this.Controls.Add(this.botonMAS);
+            this.Controls.Add(this.botonIGUAL);
+            this.Controls.Add(this.botonX);
+            this.Controls.Add(this.botonDEL);
+            this.Controls.Add(this.botonpunto);
+            this.Controls.Add(this.boton2);
+            this.Controls.Add(this.boton5);
+            this.Controls.Add(this.boton8);
+            this.Controls.Add(this.boton1);
+            this.Controls.Add(this.boton0);
+            this.Controls.Add(this.boton4);
+            this.Controls.Add(this.boton7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button boton7;
+        private System.Windows.Forms.Button boton4;
+        private System.Windows.Forms.Button boton1;
+        private System.Windows.Forms.Button boton0;
+        private System.Windows.Forms.Button botonpunto;
+        private System.Windows.Forms.Button boton2;
+        private System.Windows.Forms.Button boton5;
+        private System.Windows.Forms.Button boton8;
+        private System.Windows.Forms.Button botonANS;
+        private System.Windows.Forms.Button boton3;
+        private System.Windows.Forms.Button boton6;
+        private System.Windows.Forms.Button boton9;
+        private System.Windows.Forms.Button botonMAS;
+        private System.Windows.Forms.Button botonIGUAL;
+        private System.Windows.Forms.Button botonX;
+        private System.Windows.Forms.Button botonDEL;
+        private System.Windows.Forms.Button botonMEN;
+        private System.Windows.Forms.Button botonHISTORY;
+        private System.Windows.Forms.Button botonDIV;
+        private System.Windows.Forms.Button botonAC;
+        private System.Windows.Forms.TextBox textbox;
+    }
+}
+
---
+++ Proyecto7Neira/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto7Neira
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void boton8_Click(object sender, EventArgs e)
+        {
+            textbox.Text = textbox.Text + "8";
+        }
+
+        private void boton2_Click(object sender, EventArgs e)
+        {
+            textbox.Text = textbox.Text + "2";
+        }
+        private void boton4_Click(object sender, EventArgs e)
+        {
+            textbox.Text = textbox.Text + "4";
+        }
+
+        private void boton5_Click(object sender, EventArgs e)
+        {
+            textbox.Text = textbox.Text + "5";
+        }
+
+        private void botonpunto_Click(object sender, EventArgs e)
+        {
+            textbox.Text = textbox.Text + ".";
+        }
+
+        private void botonHist_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botonRes_Click(object sender, EventArgs e)
+        {
+            textbox.Text = textbox.Text + "-";
+        }
+
+        private void botondiv_Click(object sender, EventArgs e)
+        {
+            textbox.Text = textbox.Text + "/";
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boton3_Click(object sender, EventArgs e)
+        {
+            textbox.Text = textbox.Text + "3";
+        }
+
+        private void boton6_Click(object sender, EventArgs e)
+        {
+            textbox.Text = textbox.Text + "6";
+        }
+
+        private void boton9_Click(object sender, EventArgs e)
+        {
+            textbox.Text = textbox.Text + "9";
+        }
+
+        private void botonMAS_Click(object sender, EventArgs e)
+        {
+            textbox.Text = textbox.Text + "+";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botonx_Click(object sender, EventArgs e)
+        {
+            textbox.Text = textbox.Text + "x";
+        }
+
+        private void botonDEL_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void boton1_Click(object sender, EventArgs e)
+        {
+            textbox.Text = textbox.Text + "1";
+        }
+        
+        private void boton0_Click(object sender, EventArgs e)
+        {
+            textbox.Text = textbox.Text + "0";
+        }
+
+        private void boton7_Click(object sender, EventArgs e)
+        {
+            textbox.Text = textbox.Text + "7";
+        }
+    }
+}
